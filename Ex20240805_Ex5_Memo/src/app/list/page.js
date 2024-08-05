b0f94fd2068a0b1ecbf4fd0471081bd6f5b87032
wjsxorjs <wjsxorjs@naver.com>
2024-08-05 14:55:22 +0900

    20240805 NextJS 0.5

--- Ex20240805_Ex5_Memo/src/app/list/page.js
+++ Ex20240805_Ex5_Memo/src/app/list/page.js
@@ -1,30 +1,37 @@
-import { Card, CardContent } from "@mui/material"
+"use client"
+
+import MemoCard from "@/component/MemoCard"
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 export default function page() {
 
-    let m_ar=[
-        {title:"안녕하세요",date:"2024-08-01",content:"잘 되나?"},
-        {title:"감사해요",date:"2024-08-02",content:"잘 되네"},
-        {title:"잘있어요",date:"2024-08-03",content:"잘 돼?"},
-        {title:"다시 만나요",date:"2024-08-04",content:"잘 된다고!"},
-    ]
+    // let m_ar=[
+    //     {title:"안녕하세요",date:"2024-08-01",content:"잘 되나?"},
+    //     {title:"감사해요",date:"2024-08-02",content:"잘 되네"},
+    //     {title:"잘있어요",date:"2024-08-03",content:"잘 돼?"},
+    //     {title:"다시 만나요",date:"2024-08-04",content:"잘 된다고!"},
+    // ]
 
+    const [m_ar, setM_ar] = useState([]);
+
+    const API_URL = "/memo/list";
+
+    function getData(){
+        axios.get(API_URL).then((res)=>{
+            setM_ar(res.data.m_ar);
+        });
+    }
+
+
+    useEffect(()=>{
+        // 익명함수 내부
+        getData(); // 서버 비동기식 통신
+    },[]);
 
     return (
         <div className="list_bg">
-            <Card sx ={{maxWidth: 500}}>
-                <CardContent>
-                {m_ar.map((mvo,idx) => {
-                    return(
-                        <div key={idx} className="list_item">
-                            <h4 className="list_item-h4">{mvo.title}</h4>
-                            <p  className="list_item-p">{mvo.date}</p>
-                            <p>{mvo.content}</p>
-                        </div>
-                    )
-                })}
-                </CardContent>
-            </Card>
+            <MemoCard m_ar={m_ar} />
         </div>
     )
 }
